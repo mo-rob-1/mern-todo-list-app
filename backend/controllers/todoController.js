85767324365a2b6ba6ff784e05d4d342c3ac767e
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-19 17:43:59 +0000

    add crud functions

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,43 +1,65 @@
 const asyncHandler = require("express-async-handler");
 
+const Todo = require("../models/todoModel");
+
 // @desc  Get todos
 // @route GET /api/todo
 // @access Private
 const getTodos = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Get todos",
-  });
+  const todos = await Todo.find();
+
+  res.status(200).json(todos);
 });
 
 // @desc  Set todo
 // @route POST /api/todo
 // @access Private
 const setTodo = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  if (!req.body.todo) {
     res.status(400);
     throw new Error("Text is required");
   }
 
-  res.status(200).json({
-    message: "Set todo",
+  const todo = await Todo.create({
+    todo: req.body.todo,
   });
+
+  res.status(200).json(todo);
 });
 
 // @desc  Update todo
 // @route PUT /api/todo/:id
 // @access Private
 const updateTodo = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `Update todo ${req.params.id}`,
+  const todo = await Todo.findByIdAndUpdate(req.params.id);
+
+  if (!todo) {
+    res.status(400);
+    throw new Error("Todo not found");
+  }
+
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
   });
+
+  res.status(200).json(updatedTodo);
 });
 
 // @desc  Delete todo
 // @route DELETE /api/todo/:id
 // @access Private
 const deleteTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  if (!todo) {
+    res.status(400);
+    throw new Error("Todo not found");
+  }
+
+  await todo.remove();
+
   res.status(200).json({
-    message: `Delete todo ${req.params.id}`,
+    id: req.params.id,
   });
 });
 
