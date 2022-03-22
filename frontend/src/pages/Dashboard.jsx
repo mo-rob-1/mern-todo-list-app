48f6acec8d14681f5e35d3ff12b654f2f616ca3c
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-22 22:13:43 +0000

    add dashboard content

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import TodoForm from "../components/TodoForm";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  // Navigates user to login page if user is not logged in
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Your Todo List</p>
+      </section>
+
+      <TodoForm />
+    </>
+  );
 }
 
 export default Dashboard;
