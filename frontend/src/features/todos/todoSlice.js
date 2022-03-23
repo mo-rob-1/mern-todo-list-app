66ba5500db4ed4d843089fd77e137a59d49cf998
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-23 15:50:49 +0000

    add slice for get todos and delete todo item

--- frontend/src/features/todos/todoSlice.js
+++ frontend/src/features/todos/todoSlice.js
@@ -21,29 +21,29 @@ export const createTodo = createAsyncThunk("todos/create", async (todoData, thun
   }
 });
 
-// // Get user goals
-// export const getGoals = createAsyncThunk("goals/getAll", async (_, thunkAPI) => {
-//   try {
-//     const token = thunkAPI.getState().auth.user.token;
-//     return await goalService.getGoals(token);
-//   } catch (error) {
-//     const message =
-//       (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
-//     return thunkAPI.rejectWithValue(message);
-//   }
-// });
+// Get user todos
+export const getTodos = createAsyncThunk("todos/getAll", async (_, thunkAPI) => {
+  try {
+    const token = thunkAPI.getState().auth.user.token;
+    return await todoService.getTodos(token);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
 
-// // Delete user goal
-// export const deleteGoal = createAsyncThunk("goals/delete", async (id, thunkAPI) => {
-//   try {
-//     const token = thunkAPI.getState().auth.user.token;
-//     return await goalService.deleteGoal(id, token);
-//   } catch (error) {
-//     const message =
-//       (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
-//     return thunkAPI.rejectWithValue(message);
-//   }
-// });
+// Delete user todo
+export const deleteTodo = createAsyncThunk("todos/delete", async (id, thunkAPI) => {
+  try {
+    const token = thunkAPI.getState().auth.user.token;
+    return await todoService.deleteTodo(id, token);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
 
 export const todoSlice = createSlice({
   name: "todo",
@@ -62,6 +62,32 @@ export const todoSlice = createSlice({
         state.todos.push(action.payload);
       })
       .addCase(createTodo.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getTodos.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getTodos.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.todos = action.payload;
+      })
+      .addCase(getTodos.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(deleteTodo.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteTodo.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.todos = state.todos.filter((todo) => todo._id !== action.payload.id);
+      })
+      .addCase(deleteTodo.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
